90924126a0491c1eedca185567eab6df8c239a78
Håvard Dahl <hdahl1811@gmail.com>
2024-09-18 21:14:59 +0200

    Workspace demo

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "rg_name" {
+    type = string
+    description = "The name of the resourse group in terraform"
+}
+
+variable "rg_location" {
+    type = string
+    description = "The location of the resource group in terraform"
+  
+}
